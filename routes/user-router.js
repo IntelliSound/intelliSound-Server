0a991133a2f5f73fb709a856f20f94a7eca069e2
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-05 10:38:02 -0800

    setting up basic files for user route and tests

---
+++ routes/user-router.js
@@ -0,0 +1,9 @@
+const {Router} = require('express');
+const jsonParser = require('body-parser').json();
+const User = require('../models/user');
+
+const userRouter = new Router();
+
+userRouter.post();
+
+userRouter.get();
