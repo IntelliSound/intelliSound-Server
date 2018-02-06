2323bd5de4c0d6d18fe65fff61cc5f591e0f8042
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-05 18:34:30 -0800

    add multer to neural network router

--- routes/neuralNetwork-router.js
+++ routes/neuralNetwork-router.js
@@ -3,12 +3,14 @@
 const {Router} = require('express');
 const jsonParser = require('body-parser').json();
 const httpErrors = require('http-errors');
+const multer = require(multer);
+const upload = multer({dest: `${__dirname}/../temp`});
 const bearerAuthMiddleware = require('../lib/middleware/bearer-middleware');
 
 const neuralNetworkRouter = module.exports = new Router();
 
 // user must be logged in to perform any actions on a saved network
-neuralNetworkRouter.post(`/network`, bearerAuthMiddleware, (request, response, next) => {
+neuralNetworkRouter.post(`/network`, bearerAuthMiddleware, upload.any(), (request, response, next) => {
 
 });
 neuralNetworkRouter.get('/network/:networkID');
