0127d8123005846f443bf126348b6ee9969b5ead
Andrew Bloom <blooma116@gmail.com>
2018-02-08 11:52:41 -0800

    refactored post to correctly handle updating neural net on user model

--- routes/neuralNetwork-router.js
+++ routes/neuralNetwork-router.js
@@ -14,11 +14,13 @@ const waveWriter = require('../lib/wave-writer');
 
 const neuralNetworkRouter = module.exports = new Router();
 
+neuralNetworkRouter.post(`/neuralnetwork/save/`);
+
 // user must be logged in to perform any actions on a saved network/save a network
 neuralNetworkRouter.post(`/neuralnetwork/:wavename/:neuralnetname`, jsonParser, bearerAuthMiddleware, (request, response, next) => {
   // need to add the neuralNetwork created through WaveRouter to the user's array of networks
 
-  // Check where this error is handled or if it needs to be refactored
+  // Andrew - TODO: check where this error is handled or if it needs to be refactored
   if (!request.params.wavename){
     throw new httpErrors('__ERROR__', 'User must select a wave to use');
   }
@@ -29,7 +31,8 @@ neuralNetworkRouter.post(`/neuralnetwork/:wavename/:neuralnetname`, jsonParser, 
   const path = `${__dirname}/../assets/${request.params.wavename}.wav`;
   let neuralGeneratedFile = null;
   let newNeuralNetwork = null;
-
+  
+  //Nicholas- put the wav file through a new neural net. then add its id to the user object and update user
   return fsx.readFile(path)
     .then(data => {
       let parsedFile = waveParser(data);
@@ -42,19 +45,12 @@ neuralNetworkRouter.post(`/neuralnetwork/:wavename/:neuralnetname`, jsonParser, 
       }).save()
         .then(network => {
           newNeuralNetwork = network;
-          //Nicholas- put the wav file through a new neural net. then add its id to the user object and update user
-          return User.findOne({_id: request.user._id})
-            .then(user => {
-        
-              logger.log(user, `user in the actual router`);
-              user.neuralNetworks.push(newNeuralNetwork._id);
-            }).save()
+          request.user.neuralNetworks.push(newNeuralNetwork._id);
+          return User.findByIdAndUpdate(request.user._id, request.user)
             .then(() => response.json({newNeuralNetwork, neuralGeneratedFile}))
             .catch(next);
-
         });
     });
-        
 });
 
 
